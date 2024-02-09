e9e7807aedc6c53e194d5c604da7114870de40bc
ruvolpe <ru.volpe@gmail.com>
2024-02-09 17:43:29 +0000

    feat: finished ch2

--- learningPython/learning-python-2896241/Ch2 - Basics/modules_start.py
+++ learningPython/learning-python-2896241/Ch2 - Basics/modules_start.py
@@ -1,14 +1,13 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
-
 # TODO: import the math module, which contains features for working with mathematics
-
+import math
 
 # TODO: the math module contains lots of pre-built functions
-
+print(math.sqrt(16))
 
 # TODO: in addition to functions, some modules contain useful constants 
-
+print(math.pi)
 
 # TODO: try some of the math functions for yourself here:
